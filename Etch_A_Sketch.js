c09aeff59651542c28dbc96f4e4e699520e5d5a7
Lawson Lewallen <150570886+lawsonlewallen@users.noreply.github.com>
2024-07-18 12:48:40 -0800

    Basic requirements
    This main branch meets the initial requirements for the Etch-A-Sketch project. Some minor CSS styling done to mimic the toy, definitely could use more. Would like to try recoding and debugging the event listener for the drawing feature to use a forEach method for the children rather than relying on event delegation through the parent.

---
+++ Etch_A_Sketch.js
@@ -0,0 +1,46 @@
+//global variables
+let gridParent = document.querySelector("#gridparent");
+let div = document.querySelector("div");
+let button = document.querySelector("button");
+
+//declaring functions to make the page work
+function createError(){
+        alert('Choose a number less than or equal to 100!')
+}
+
+//builds new grid
+function createGrid(number){
+    if (number > 100) {
+        createError();}
+        else {
+    for (let i = 0; i < (number*number); i++){
+    let gridChild = document.createElement("div");
+    gridChild.style.cssText = `margin: 0; padding: 0; width: ${450/number}px; height: ${450/number}px;`;
+    gridChild.classList.add('unrolled');
+    gridParent.appendChild(gridChild);
+}}   
+}
+
+//removes old grid
+function clearGrid() {
+    while (gridParent.firstChild) {
+        gridParent.removeChild(gridParent.lastChild);
+      }
+}
+
+//fires when button is clicked, takes userinput and fires clearGrid and createGrid
+function getUserInput() {
+    let userInput = prompt("How many squares tall and wide do you want your Etch-A-Sketch to be?", "0-100");
+    clearGrid();
+    createGrid(userInput);
+}
+
+//event listener to run createGrid when button is clicked
+button.addEventListener("onclick", getUserInput())
+
+//event listener, listens to parent and affects the targeted child
+gridParent.addEventListener("mouseover", (event) => {
+    event.target.classList.replace('unrolled', 'rolledover')});
+
+
+
